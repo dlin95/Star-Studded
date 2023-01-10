77061457096e7dce11dad0351631fe4dd11b2397
Darren Lin <darrenlin815@hotmail.com>
2023-01-10 14:27:48 -0800

    Added backend code, running the server and fetching the data from the db

---
+++ server/src/app.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const app = express();
+
+const db = require('./db');
+
+const users = require('./routes/users');
+
+function read(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      file,
+      {
+        encoding: 'utf-8',
+      },
+      (error, data) => {
+        if (error) return reject(error);
+        resolve(data);
+      }
+    );
+  });
+}
+
+module.exports = (ENV) => {
+  app.use(cors());
+  app.use(helmet());
+  app.use(express.json);
+
+  app.use('/api', users(db));
+
+
+  if (ENV === 'development') {
+    Promise.all([
+      read(path.resolve(__dirname, `db/schema/01_schema.sql`)),
+      read(path.resolve(__dirname, `db/seeds/01_seeds.sql`))
+    ])
+      .then(([create, seed]) => {
+        app.get('/api/debug/reset', (request, response) => {
+          db.query(create)
+            .then(() => db.query(seed))
+            .then(() => {
+              console.log('Database Reset');
+              response.status(200).send('Database Reset');
+          });
+        });
+      })
+      .catch(error => {
+        console.log(`Error setting up the reset route: ${error}`);
+      });
+  }
+
+  app.close = () => {
+    return db.end();
+  };
+
+  return app;
+};
---
+++ server/src/index.js
@@ -0,0 +1,9 @@
+const PORT = process.env.PORT || 3001
+const ENV = require('./environment');
+
+const app = require('./app')(ENV);
+const server = require('http').Server(app);
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT} in ${ENV} mode.`);
+});
