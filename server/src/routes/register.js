be13e997bc9990c12a9252fafa6e34c8b5dca6fd
Darren Lin <darrenlin815@hotmail.com>
2023-01-10 21:25:16 -0800

    Added register route

---
+++ server/src/routes/register.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+module.exports = (db) => {
+  router.post(`/users`, (req, res) => {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedpassword = bcrypt.hashSync(req.body.password, salt);
+    db.query(
+      `
+      INSERT INTO users
+      (first_name, last_name, email, password, profile_photo_url)
+      VALUES
+      ('${req.body.first_name}', '${req.body.last_name}', '${req.body.email}', '${hashedpassword}', '${profile_photo_url}')
+      `
+    )
+      .then((result) => {
+        console.log('New user successfully added');
+        res.json({
+          statuscode: 200,
+          message: 'User was successfully created'
+        });
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      });
+  });
+  return router;
+};
