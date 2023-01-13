3d00cf76463a93ce8fde23d39cebd5a2d9e135d7
Darren Lin <darrenlin815@hotmail.com>
2023-01-13 04:38:13 -0800

    Adds movies-list page to app alongside elements to display movie posters in a grid

--- client/src/components/MoviesList/MoviesList.js
+++ client/src/components/MoviesList/MoviesList.js
@@ -12,14 +12,14 @@ class moviesList extends Component {
     movies: [],
     loading: false,
     searchTerm: ''
-  }
+  };
 
   componentDidMount() {
     if (localStorage.getItem('homeState')) {
       const state = JSON.parse(localStorage.getItem('homeState'));
-      this.setState({...state});
+      this.setState({ ...state });
     } else {
-      this.setState({loading: true});
+      this.setState({ loading: true });
       const endPoint = `${API_URL}trending/movie/week?api_key=${API_KEY}`;
       this.fetchItems(endPoint);
     }
@@ -33,40 +33,40 @@ class moviesList extends Component {
           movies: [...this.state.movies, ...result.results],
           loading: false
         }, () => {
-          if(this.state.searchTerm === ''){
-            localStorage.setItem('homeState', JSON.stringify(this.state))
+          if (this.state.searchTerm === '') {
+            localStorage.setItem('homeState', JSON.stringify(this.state));
           }
-        })
-      })
-  }
+        });
+      });
+  };
 
-    render() {
-      return (
-        <main className='main-container'>
-          <img
-            src='/background.jpeg'
-            alt='StarStudded Movies Background'
-            className='mainBackground'
-          />
-          <div className='movie-grid'>
-            <FourColumnGrid 
-              header={this.state.searchTerm ? 'Search Result' : 'All Movies'}
-              loading={this.state.loading}
-              >
-                {this.state.movies.map((element, i) => {
-                  return <MovieThumbnail
-                  key={i}
-                  clickable={true}
-                  image={`${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}`}
-                  movieId={element.id}
-                  movieName={element.original_title}
-                  />
-                })}
-            </FourColumnGrid>
-          </div>
-        </main>
-      )
-    }
+  render() {
+    return (
+      <main className='main-container'>
+        <img
+          src='/background.jpeg'
+          alt='StarStudded Movies Background'
+          className='mainBackground'
+        />
+        <div className='movie-grid'>
+          <FourColumnGrid
+            header={this.state.searchTerm ? 'Search Result' : 'All Movies'}
+            loading={this.state.loading}
+          >
+            {this.state.movies.map((element, i) => {
+              return <MovieThumbnail
+                key={i}
+                clickable={true}
+                image={`${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}`}
+                movieId={element.id}
+                movieName={element.original_title}
+              />;
+            })}
+          </FourColumnGrid>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default moviesList;
