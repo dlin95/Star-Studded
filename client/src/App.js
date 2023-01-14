80e1e079c80e3da6827b834c04dea287f15aeb0d
Maris <mcclapperton@gmail.com>
2023-01-14 11:44:41 -0700

    add movie details component

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,33 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import ErrorPage from './error-page';
-import Main from './pages/Main';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import FriendsSuggestions from './pages/FriendsSuggestions';
-import MovieDetail from './pages/MovieDetail';
-import MoviesList from './components/MoviesList/MoviesList';
-import WatchList from './pages/WatchList';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import ErrorPage from "./error-page";
+import Main from "./pages/Main";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import FriendsSuggestions from "./pages/FriendsSuggestions";
+import MovieDetails from "./components/MovieDetaisl";
+import MoviesList from "./components/MoviesList/MoviesList";
+import WatchList from "./pages/WatchList";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route path="/" element={<Main />} />
         <Route index element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/friend-suggestion' element={<FriendsSuggestions />} />
-        <Route path='/movie-detail' element={<MovieDetail />} />
-        <Route path='/movies-list' element={<MoviesList />} />
-        <Route path='/watch-list' element={<WatchList />} />
-        <Route path="*" element={< ErrorPage />}>
-        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/friend-suggestion" element={<FriendsSuggestions />} />
+        <Route path="/movie-details" element={<MovieDetails />} />
+        <Route path="/movies-list" element={<MoviesList />} />
+        <Route path="/watch-list" element={<WatchList />} />
+        <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
-    </div >
+    </div>
   );
 }
 
---
+++ client/src/components/movieDetails.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const MovieDetails = (props) => {
+  const imageUrl = "https://image.tmdb.org/t/p/w500/" + props.poster_path;
+
+  return (
+    <div className="card">
+      <img src={imageUrl} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <div className="d-flex justify-content-between">
+          <small>{props.release_date}</small>
+          <span className="badge rounded-pill text-bg-warning">
+            <i class="fa-solid fa-star me-1"></i>
+            {props.vote_average.toFixed(1)}
+          </span>
+        </div>
+        <div className="mt-2 d-flex justify-content-between align-items-center">
+          <button type="button" className="btn btn-light mt-1">
+            <i className="fa-regular fa-plus me-1"></i>Watchlist
+          </button>
+          <i className="fa-regular fa-2x fa-heart"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieDetails;
