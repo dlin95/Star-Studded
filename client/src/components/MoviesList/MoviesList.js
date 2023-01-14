3f84f619b92796c3045a74ac51e0d95f4a82c6c4
Darren Lin <darrenlin815@hotmail.com>
2023-01-14 02:18:37 -0800

    Adds a Show More button to extend the list of movie posters

--- client/src/components/MoviesList/MoviesList.js
+++ client/src/components/MoviesList/MoviesList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { API_KEY, API_URL, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../../config';
 import FourColumnGrid from '../elements/FourColumnGrid/FourColumnGrid';
 import MovieThumbnail from '../elements/MovieThumbnail/MovieThumbnail';
+import ShowMoreBtn from '../elements/ShowMoreBtn/ShowMoreBtn';
 // import { Link } from 'react-router-dom';
 import './MoviesList.scss';
 
@@ -11,6 +12,8 @@ class moviesList extends Component {
   state = {
     movies: [],
     loading: false,
+    currentPage: 0,
+    totalPages: 0,
     searchTerm: ''
   }
 
@@ -23,6 +26,18 @@ class moviesList extends Component {
       const endPoint = `${API_URL}trending/movie/week?api_key=${API_KEY}`;
       this.fetchItems(endPoint);
     }
+  }
+
+  loadMoreMovies = () => {
+    let endpoint = '';
+    this.setState({loading: true});
+
+    if(this.state.searchTerm = ''){
+      endpoint = `${API_URL}trending/movie/week?api_key=${API_KEY}${this.state.currentPage + 1}`;
+    } else {
+      endpoint = `${API_URL}trending/movie/week?api_key=${API_KEY}${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+    }
+    this.fetchItems(endpoint);
   }
 
   fetchItems = (endpoint) => {
@@ -40,33 +55,32 @@ class moviesList extends Component {
       })
   }
 
-    render() {
-      return (
-        <main className='main-container'>
-          <img
-            src='/background.jpeg'
-            alt='StarStudded Movies Background'
-            className='mainBackground'
-          />
-          <div className='movie-grid'>
-            <FourColumnGrid 
-              header={this.state.searchTerm ? 'Search Result' : 'All Movies'}
-              loading={this.state.loading}
-              >
-                {this.state.movies.map((element, i) => {
-                  return <MovieThumbnail
-                  key={i}
-                  clickable={true}
-                  image={`${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}`}
-                  movieId={element.id}
-                  movieName={element.original_title}
-                  />
-                })}
-            </FourColumnGrid>
-          </div>
-        </main>
-      )
-    }
+  render() {
+    return (
+      <div className='home'>
+        <div className='movie-grid'>
+          <FourColumnGrid 
+            header={this.state.searchTerm ? 'Search Result' : 'All Movies'}
+            loading={this.state.loading}
+            >
+              {this.state.movies.map((element, i) => {
+                return <MovieThumbnail
+                key={i}
+                clickable={true}
+                image={`${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}`}
+                movieId={element.id}
+                movieName={element.original_title}
+                />
+              })}
+          </FourColumnGrid>
+            {(this.state.currentPage <= this.state.totalPages && !this.state.loading) ?
+              <ShowMoreBtn text={'Show More'} onClick={this.loadMoreMovies} />
+              : null
+            }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default moviesList;
--- client/src/config.js
+++ client/src/config.js
@@ -5,7 +5,7 @@ const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 
 const BACKDROP_SIZE = 'w1280';
 
-const POSTER_SIZE = 'w500';
+const POSTER_SIZE = 'w300';
 
 export {
   API_URL,
