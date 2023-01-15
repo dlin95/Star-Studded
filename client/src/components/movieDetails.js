f96c335c70f8b95cdfd257b042f7ed66a9475acb
Maris <mcclapperton@gmail.com>
2023-01-15 02:12:57 -0700

    update styles for movieDetails

--- client/src/components/movieDetails.js
+++ client/src/components/movieDetails.js
@@ -6,7 +6,7 @@ const MovieDetails = (props) => {
   props = props.movie;
   const imageUrl = "https://image.tmdb.org/t/p/w500/" + props.poster_path;
   console.log(props);
-  console.log("hi mars");
+  console.log("hi there");
   return (
     <>
       <Navbar />
@@ -14,20 +14,24 @@ const MovieDetails = (props) => {
         <div className="card">
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h2 className="card-title">{props.title}</h2>
-            <p className="card-overview">{props.overview}</p>
-            <h5 className="card-runtime">Runtime: {props.runtime} minutes</h5>
-            <h5 className="card-tagline">"{props.tagline}"</h5>
+            <h2 className="card-descript title">{props.title}</h2>
+            <p className="card-descript overview">{props.overview}</p>
+            <h5 className="card-descript runtime">
+              Runtime: {props.runtime} minutes
+            </h5>
+            <h5 className="card-descript tagline">"{props.tagline}"</h5>
             {props.genres.map((genre) => (
-              <p className="card-genre" key={genre}>
+              <p className="card-descript genre" key={genre}>
                 {genre}
               </p>
             ))}
             <div className="d-flex justify-content-between">
-              <small>Release date: {props.release_date}</small>
+              <small className="card-descript release-date">
+                Release date: {props.release_date}
+              </small>
               <span className="badge rounded-pill text-bg-warning">
                 <i className="fa-solid fa-star me-1"></i>
-                {props.vote_average}
+                {props.vote_average.toFixed(1)}
               </span>
             </div>
             <div className="mt-2 d-flex justify-content-between align-items-center">
