2c07f2dc3d4836a12c7f0ff19c112f06ae3c615f
Maris <mcclapperton@gmail.com>
2023-01-14 22:04:31 -0700

    add some styling for movieDetails

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Main from "./pages/Main";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import FriendsSuggestions from "./pages/FriendsSuggestions";
-import MovieDetails from "./components/MovieDetaisl";
+import MovieDetails from "./components/movieDetails";
 import MoviesList from "./components/MoviesList/MoviesList";
 import WatchList from "./pages/WatchList";
 import Login from "./pages/Login";
@@ -22,7 +22,85 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/friend-suggestion" element={<FriendsSuggestions />} />
-        <Route path="/movie-details" element={<MovieDetails />} />
+        <Route
+          path="/movie-details"
+          element={
+            <MovieDetails
+              movie={{
+                adult: false,
+                backdrop_path: "/nlCHUWjY9XWbuEUQauCBgnY8ymF.jpg",
+                belongs_to_collection: {},
+                budget: 150000000,
+                genres: ["urMom", "horror", "pleb"],
+                homepage: "https://www.warnerbros.com/movies/mad-max-fury-road",
+                id: 76341,
+                imdb_id: "tt1392190",
+                original_language: "en",
+                original_title: "Mad Max: Fury Road",
+                overview:
+                  "An apocalyptic story set in the furthest reaches of our planet, in a stark desert landscape where humanity is broken, and most everyone is crazed fighting for the necessities of life. Within this world exist two rebels on the run who just might be able to restore order.",
+                popularity: 73.907,
+                poster_path: "/8tZYtuWezp8JbcsvHYO0O46tFbo.jpg",
+                production_companies: [
+                  {
+                    id: 79,
+                    logo_path: "/tpFpsqbleCzEE2p5EgvUq6ozfCA.png",
+                    name: "Village Roadshow Pictures",
+                    origin_country: "US",
+                  },
+                  {
+                    id: 174,
+                    logo_path: "/IuAlhI9eVC9Z8UQWOIDdWRKSEJ.png",
+                    name: "Warner Bros. Pictures",
+                    origin_country: "US",
+                  },
+                  {
+                    id: 2537,
+                    logo_path: null,
+                    name: "Kennedy Miller Productions",
+                    origin_country: "AU",
+                  },
+                  {
+                    id: 41624,
+                    logo_path: "/wzKxIeQKlMP0y5CaAw25MD6EQmf.png",
+                    name: "RatPac Entertainment",
+                    origin_country: "US",
+                  },
+                ],
+                production_countries: [
+                  {
+                    iso_3166_1: "AU",
+                    name: "Australia",
+                  },
+                  {
+                    iso_3166_1: "US",
+                    name: "United States of America",
+                  },
+                  {
+                    iso_3166_1: "ZA",
+                    name: "South Africa",
+                  },
+                ],
+                release_date: "2015-05-13",
+                revenue: 378858340,
+                runtime: 121,
+                spoken_languages: [
+                  {
+                    english_name: "English",
+                    iso_639_1: "en",
+                    name: "English",
+                  },
+                ],
+                status: "Released",
+                tagline: "What a Lovely Day.",
+                title: "Mad Max: Fury Road",
+                video: false,
+                vote_average: 7.569,
+                vote_count: 20142,
+              }}
+            />
+          }
+        />
         <Route path="/movies-list" element={<MoviesList />} />
         <Route path="/watch-list" element={<WatchList />} />
         <Route path="*" element={<ErrorPage />}></Route>
--- client/src/components/movieDetails.js
+++ client/src/components/movieDetails.js
@@ -1,28 +1,43 @@
 import React from "react";
+import "./movieDetails.scss";
 
 const MovieDetails = (props) => {
+  props = props.movie;
   const imageUrl = "https://image.tmdb.org/t/p/w500/" + props.poster_path;
-
+  console.log(props);
+  console.log("hi mars");
   return (
-    <div className="card">
-      <img src={imageUrl} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
-        <div className="d-flex justify-content-between">
-          <small>{props.release_date}</small>
-          <span className="badge rounded-pill text-bg-warning">
-            <i class="fa-solid fa-star me-1"></i>
-            {props.vote_average.toFixed(1)}
-          </span>
-        </div>
-        <div className="mt-2 d-flex justify-content-between align-items-center">
-          <button type="button" className="btn btn-light mt-1">
-            <i className="fa-regular fa-plus me-1"></i>Watchlist
-          </button>
-          <i className="fa-regular fa-2x fa-heart"></i>
+    <main className="bigcard">
+      <div className="card">
+        <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h3 className="card-title">{props.title}</h3>
+          <h6 className="card-overview">{props.overview}</h6>
+          <h3 className="card-runtime">Runtime: {props.runtime} minutes</h3>
+          <h5 className="card-tagline">"{props.tagline}"</h5>
+          <h6 className="card-homepage">{props.homepage}</h6>
+          <h3>Genres:</h3>
+          {props.genres.map((genre) => (
+            <h4 className="card-genre" key={genre}>
+              {genre}
+            </h4>
+          ))}
+          <div className="d-flex justify-content-between">
+            <small>Release date: {props.release_date}</small>
+            <span className="badge rounded-pill text-bg-warning">
+              <i className="fa-solid fa-star me-1"></i>
+              {props.vote_average}
+            </span>
+          </div>
+          <div className="mt-2 d-flex justify-content-between align-items-center">
+            <button type="button" className="btn btn-light mt-1">
+              <i className="fa-regular fa-plus me-1"></i>Watchlist
+            </button>
+            <i className="fa-regular fa-2x fa-heart"></i>
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
