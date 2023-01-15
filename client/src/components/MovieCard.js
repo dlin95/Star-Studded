dafde55500c78145a5905837d4cd8db46f907ef6
Maris <mcclapperton@gmail.com>
2023-01-15 02:42:59 -0700

    corrected add to movie-details instead of movie card

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,27 +1,8 @@
 import React from "react";
-import axios from "axios";
-import { toast } from "react-toastify";
+
 
 const MovieCard = (props) => {
-  const handleAddToWatchList = async function (e, movie_id) {
-    e.preventDefault();
-    const currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
-    const watchlist = {
-      user_id: currentUser.id,
-      movie_id: movie_id,
-    };
-
-    return await axios
-      .post("/api/watchlist", watchlist)
-      .then((result) => {
-        console.log("Added to Watchlist");
-        toast.success("Added successfully to Watchlist");
-      })
-      .catch((error) => {
-        toast.error("Failed to Add to WatchList");
-      });
-  };
-
+  
   const imageUrl = "https://image.tmdb.org/t/p/w500/" + props.poster_path;
 
   return (
@@ -37,13 +18,6 @@ const MovieCard = (props) => {
           </span>
         </div>
         <div className="mt-2 d-flex justify-content-between align-items-center">
-          <button
-            type="button"
-            className="btn btn-light mt-1"
-            onClick={(e) => handleAddToWatchList(e, props.id)}
-          >
-            <i className="fa-regular fa-plus me-1"></i>Watchlist
-          </button>
           <i className="fa-regular fa-2x fa-heart"></i>
         </div>
       </div>
--- client/src/components/movieDetails.js
+++ client/src/components/movieDetails.js
@@ -1,10 +1,33 @@
 import React from "react";
 import "./movieDetails.scss";
 import Navbar from "./Navbar";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const MovieDetails = (props) => {
+  const handleAddToWatchList = async function (e, movie_id) {
+    e.preventDefault();
+    const currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+    const watchlist = {
+      user_id: currentUser.id,
+      movie_id: movie_id,
+    };
+
+    return await axios
+      .post("/api/watchlist", watchlist)
+      .then((result) => {
+        console.log("Added to Watchlist");
+        toast.success("Added successfully to Watchlist");
+      })
+      .catch((error) => {
+        toast.error("Failed to Add to WatchList");
+      });
+  };
+
   props = props.movie;
+
   const imageUrl = "https://image.tmdb.org/t/p/w500/" + props.poster_path;
+
   console.log(props);
   console.log("hi there");
   return (
@@ -35,7 +58,11 @@ const MovieDetails = (props) => {
               </span>
             </div>
             <div className="mt-2 d-flex justify-content-between align-items-center">
-              <button type="button" className="btn btn-light mt-1">
+              <button
+                type="button"
+                className="btn btn-light mt-1"
+                onClick={(e) => handleAddToWatchList(e, props.id)}
+              >
                 <i className="fa-regular fa-plus me-1"></i>Watchlist
               </button>
               <i className="fa-regular fa-2x fa-heart"></i>
