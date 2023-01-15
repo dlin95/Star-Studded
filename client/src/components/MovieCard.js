36a2cb866f6d5c085f6f93b2687586741f2142b0
Maris <mcclapperton@gmail.com>
2023-01-15 02:40:47 -0700

    add hetas movie card code for add watchlist

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const MovieCard = (props) => {
+  const handleAddToWatchList = async function (e, movie_id) {
+    e.preventDefault();
+    const currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+    const watchlist = {
+      user_id: currentUser.id,
+      movie_id: movie_id,
+    };
+
+    return await axios
+      .post("/api/watchlist", watchlist)
+      .then((result) => {
+        console.log("Added to Watchlist");
+        toast.success("Added successfully to Watchlist");
+      })
+      .catch((error) => {
+        toast.error("Failed to Add to WatchList");
+      });
+  };
 
   const imageUrl = "https://image.tmdb.org/t/p/w500/" + props.poster_path;
 
@@ -9,12 +29,21 @@ const MovieCard = (props) => {
       <img src={imageUrl} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
-        <div className='d-flex justify-content-between'>
+        <div className="d-flex justify-content-between">
           <small>{props.release_date}</small>
-          <span className="badge rounded-pill text-bg-warning"><i class="fa-solid fa-star me-1"></i>{props.vote_average.toFixed(1)}</span>
+          <span className="badge rounded-pill text-bg-warning">
+            <i class="fa-solid fa-star me-1"></i>
+            {props.vote_average.toFixed(1)}
+          </span>
         </div>
-        <div className='mt-2 d-flex justify-content-between align-items-center'>
-          <button type="button" className="btn btn-light mt-1"><i className="fa-regular fa-plus me-1"></i>Watchlist</button>
+        <div className="mt-2 d-flex justify-content-between align-items-center">
+          <button
+            type="button"
+            className="btn btn-light mt-1"
+            onClick={(e) => handleAddToWatchList(e, props.id)}
+          >
+            <i className="fa-regular fa-plus me-1"></i>Watchlist
+          </button>
           <i className="fa-regular fa-2x fa-heart"></i>
         </div>
       </div>
