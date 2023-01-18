bde7a1c6bc11e4d2f5eed8234f19f81e6a44c4bd
Maris <mcclapperton@gmail.com>
2023-01-18 01:26:08 -0700

    update tagling

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.scss";
 
 const Home = () => {
@@ -9,13 +9,20 @@ const Home = () => {
         <img
           src="/background.jpeg"
           alt="StarStudded Movies Backgound"
-          className="mainBackground" />
-        <section className='layout'>
+          className="mainBackground"
+        />
+        <section className="layout">
           <h1>Welcome to StarStudded</h1>
-          <h3>Make sharing movies with your friends fun.</h3>
-          <div className='mt-4'>
-            <Link to="/login" className="btn btn-primary me-4" type="button">Sign in</Link>
-            <Link to="/register" className="btn btn-primary" type="button">Sign Up</Link>
+          <h3>
+            A neat way to search for and keep track of your favourite movies.
+          </h3>
+          <div className="mt-4">
+            <Link to="/login" className="btn btn-primary me-4" type="button">
+              Sign in
+            </Link>
+            <Link to="/register" className="btn btn-primary" type="button">
+              Sign Up
+            </Link>
           </div>
         </section>
       </main>
